e12b02436a38294f23f43deb110a5279a2277c58
Sergio Bayona <sbayona@martianinteractive.com>
2016-12-20 16:00:45 -0500

    removed ar audit tracer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_audit_fields
-
-  private 
-
-  def set_audit_fields
-  Concern::Audit::Author.current = current_admin_user.id if current_admin_user
-  end
 end
