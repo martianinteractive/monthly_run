da2fb0b7ab5c786e525f11f1fd728f63b49b03e8
Sergio Bayona <sbayona@martianinteractive.com>
2017-01-15 16:57:20 -0500

    renamed to monthly run

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_management_zone_session'
+Rails.application.config.session_store :cookie_store, key: '_monthly_run_session'
