4443df96b56c9c03d27c8b1113605b25f18e25b2
Sergio Bayona <sbayona@martianinteractive.com>
2017-01-16 12:43:13 -0500

    comment out bad columns

--- db/migrate/20161220210929_remove_ar_audit_tracer.rb
+++ db/migrate/20161220210929_remove_ar_audit_tracer.rb
@@ -1,19 +1,19 @@
 class RemoveArAuditTracer < ActiveRecord::Migration
   def change
-    remove_column :accounts, :updated_by, :string
-    remove_column :accounts, :created_by, :string
-    remove_column :admin_users, :updated_by, :string
-    remove_column :admin_users, :created_by, :string
-    remove_column :leases, :updated_by, :string
-    remove_column :leases, :created_by, :string
-    remove_column :properties, :updated_by, :string
-    remove_column :properties, :created_by, :string
-    remove_column :tenants, :updated_by, :string
-    remove_column :tenants, :created_by, :string
-    remove_column :units, :updated_by, :string
-    remove_column :units, :created_by, :string
-    remove_column :users, :updated_by, :string
-    remove_column :users, :created_by, :string
+    # remove_column :accounts, :updated_by, :string
+    # remove_column :accounts, :created_by, :string
+    # remove_column :admin_users, :updated_by, :string
+    # remove_column :admin_users, :created_by, :string
+    # remove_column :leases, :updated_by, :string
+    # remove_column :leases, :created_by, :string
+    # remove_column :properties, :updated_by, :string
+    # remove_column :properties, :created_by, :string
+    # remove_column :tenants, :updated_by, :string
+    # remove_column :tenants, :created_by, :string
+    # remove_column :units, :updated_by, :string
+    # remove_column :units, :created_by, :string
+    # remove_column :users, :updated_by, :string
+    # remove_column :users, :created_by, :string
 
     add_column :accounts, :admin_user_id, :integer
     add_column :leases, :admin_user_id, :integer
