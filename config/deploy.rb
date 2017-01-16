f651ab6087fce75165221ed1eda500950b8bb886
Sergio Bayona <sbayona@martianinteractive.com>
2017-01-16 10:57:36 -0500

    fix indentation

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ set :application, "monthly_run"
 set :repo_url, "git@github.com:martianinteractive/monthly_run.git"
 set :deploy_to, "/var/www/monthly_run"
 
+set :rvm_ruby_version, '2.3.3'
+
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
