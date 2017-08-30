de492d8d4e4f9a17ee9eef33a72662c36c68a69b
Sergio Bayona <sbayona@martianinteractive.com>
2017-08-29 22:20:29 -0400

    traits

--- spec/factories/lease_factory.rb
+++ spec/factories/lease_factory.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     unit
     charges { [build(:charge)] }
     admin_user
+
+    trait :expired do
+      starts_on 2.years.ago
+      ends_on 1.year.ago
+    end
+
+    trait :current do
+      starts_on 1.years.ago
+      ends_on 1.year.from_now
+    end
   end
 end
