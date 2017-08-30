da416a5bef5d61d8c208a1fbc4017eef8b1ab06f
Sergio Bayona <sbayona@martianinteractive.com>
2017-08-29 22:20:39 -0400

    unit payments

--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Unit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:unit) { create(:unit) }
+  let(:charge) { create(:charge, lease: lease) }
+  let(:payment) { create(:payment, charge: charge, lease: lease) }
+
+  context "past payments" do
+    let!(:lease) { create(:lease, :expired, unit: unit) }
+
+    it "returns payments from past leases" do
+      expect(unit.past_payments).to eq([payment])
+    end
+  end
+
+  context "current payments" do
+    let!(:lease) { create(:lease, :current, unit: unit) }
+
+    it "returns payments from current leases" do
+      expect(unit.past_payments).to eq([payment])
+    end
+  end
+
 end
