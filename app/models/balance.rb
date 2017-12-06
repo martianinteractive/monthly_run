725c701c2e518e27ad5c3b4eb944b9ccafbd1ee2
Sergio Bayona <bayona.sergio@gmail.com>
2017-12-05 23:34:25 -0500

    balance class

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,3 +1,13 @@
 class Balance
-  
+
+  def initialize(applicable_date)
+    @applicable_date = applicable_date
+    @start_date = applicable_date.beginning_of_month
+    @end_date = applicable_date.end_of_month
+  end
+
+  def today
+    Lease.joins("LEFT JOIN payments p ON leases.id = p.lease_id AND p.applicable_period between '#{@start_date}' AND '#{@end_date}' WHERE CURRENT_DATE < leases.ends_on")
+  end
+
 end
